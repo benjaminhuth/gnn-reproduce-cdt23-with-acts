25d1638fac5db42f4478c2f8fb63922315af097b
Benjamin Huth <bhuth@acts-dev-rd-et.cern.ch>
2024-06-27 15:10:23 +0200

    update

--- get_data.sh
+++ get_data.sh
@@ -6,11 +6,18 @@ MM_PATH=/eos/atlas/atlascerngroupdisk/perf-idtracking/GNN4ITk/ATLAS-P2-RUN4-03-0
 
 MMT_FILE=$MM_PATH/ModuleMap_rel24_ttbar_v5_89809evts.triplets.root
 MMD_FILE=$MM_PATH/ModuleMap_rel24_ttbar_v5_89809evts.doublets.root
-DUMP_FILE=/eos/user/p/pibutti/sw/run/gnn/Dump_GNN4Itk.root
+DUMP_FILE=/eos/user/b/bhuth/data/Dump_GNN4Itk.root
 
 CDT23_CKPT=/eos/user/s/scaillou/CTD_2023/model_store/gnn/GNN_IN2_epochs169.ckpt
 
-sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$MMD_FILE ./data/
-sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$MMT_FILE ./data/
-sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$DUMP_FILE ./data/
-sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$CDT23_CKPT ./data/
+if false; then
+    sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$MMD_FILE ./data/
+    sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$MMT_FILE ./data/
+    sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$DUMP_FILE ./data/
+    sshpass -p "$CERN_PWD" scp bhuth@lxplus.cern.ch:$CDT23_CKPT ./data/
+else
+    cp $MMD_FILE ./data/
+    cp $MMT_FILE ./data/
+    cp $DUMP_FILE ./data/
+    cp $CDT23_CKPT ./data/
+fi
