643a85715123f73a15e35dbecb13df9792c32346
Benjamin Huth <benjamin.huth@cern.ch>
2024-09-10 15:26:45 +0200

    update

--- run_rel24_acts_with_fit.sh
+++ run_rel24_acts_with_fit.sh
@@ -15,9 +15,9 @@ try_source $HOME/setup_lcg_cuda.sh
 try_source $HOME/gnn/acts/build/python/setup.sh
 try_source $HOME/CERN/acts/build/python/setup.sh
 
-DATA=rel24/data/user.avallier.38040858.EXT0._000124.Dump_GNN4Itk.root
+#DATA=rel24/data/user.avallier.38040858.EXT0._000124.Dump_GNN4Itk.root
 #DATA=rel24/data/user.avallier.38040855.EXT0._000005.Dump_GNN4Itk.root
-#DATA=rel24/data/Dump_GNN4Itk.root
+DATA=rel24/data/Dump_GNN4Itk.root
 
 export PYTHONPATH=$HOME/itk-gen1/itk_geometry/scripts:$PYTHONPATH
 
